dc4833d8ea3c9f2fd452917133417b332cc428d8
durgaprasadbukka <bdurgaprasad15071998@gmail.com>
2025-04-02 19:39:13 -0400

    Add files via upload

---
+++ hospital sql queries data.sql
@@ -0,0 +1,14 @@
+show columns from hospinfo;
+SELECT
+    Hospital_Name,
+    Hospital_Ownership,
+    Hospital_Type,
+    State,
+    City,
+    ZIP_Code,
+Hospital_overall_rating,
+County_Name,
+Location
+
+    FROM
+	hospinfo;
